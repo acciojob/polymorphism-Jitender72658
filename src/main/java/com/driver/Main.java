445814481e6a1e4c525cadf1fc9a5a2e0bd53fe2
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-07 23:28:09 +0530

    submitting assignment solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product obj = new Product();
+        obj.product(4,5);
+        obj.product(4,5,6);
+        obj.product(4.5,5.2);
+    }
+    public static class Product{
+        public static int product(int x, int y) {
+            return  x*y;
+        }
+        public static int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public static double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
